2233cc0a4307519b13d07b3b4080985b9c6768e7
Xin Sun <accessun@hotmail.com>
2016-10-24 12:58:17 +0800

    modified encryption module

--- src/io/github/accessun/encryption/BCryptEncryption.java
+++ src/io/github/accessun/encryption/BCryptEncryption.java
@@ -1,18 +1,30 @@
 package io.github.accessun.encryption;
 
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class BCryptEncryption {
 
-    public static String encrypt(String plainPassword) {
+    public String encrypt(String plainPassword) {
         return BCrypt.hashpw(plainPassword, BCrypt.gensalt(10)); // default param is 10
     }
 
-    public static boolean checkMatch(String plaintext, String storedHash) {
+    public boolean checkMatch(String plaintext, String storedHash) {
         boolean isMatch = false;
         if (BCrypt.checkpw(plaintext, storedHash))
             isMatch = true;
         return isMatch;
     }
 
+    @Test
+    public void testBCryptPasswordEncryption() {
+        String plaintext = UUID.randomUUID().toString();
+        String encPassword = encrypt(plaintext);
+
+        Assert.assertTrue(checkMatch(plaintext, encPassword));
+        Assert.assertFalse(encPassword.equals(encrypt(plaintext)));
+    }
 }
