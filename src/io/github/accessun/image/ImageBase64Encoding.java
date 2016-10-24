89f3d52149d0eed23a2af017abee85970dc332ba
Xin Sun <accessun@hotmail.com>
2016-10-24 11:21:36 +0800

    cleaned up and re-arranged code

--- src/io/github/accessun/image/ImageBase64Encoding.java
+++ src/io/github/accessun/image/ImageBase64Encoding.java
@@ -5,12 +5,20 @@ import java.io.IOException;
 import java.util.Base64;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Test;
 
 public class ImageBase64Encoding {
     
-    public static String encode(String imagePath) throws IOException {
+    public String encode(String imagePath) throws IOException {
         String base64Image = Base64.getEncoder().encodeToString(FileUtils.readFileToByteArray(new File(imagePath)));
         final String prependStr = "data:image/png;base64, ";
         return prependStr + base64Image;
+    }
+    
+    @Test
+    public void testImageEncoding() throws IOException {
+        String imagePath = "C:/Users/User/Desktop/mytmp.PNG";
+        String encodedStr = encode(imagePath);
+        System.out.println(encodedStr);
     }
 }
